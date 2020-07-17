8eec603ea341cea0dec1cd6d3a0186be38943252
Kapil Pramod Deshmukh <44638458+SnowyKay@users.noreply.github.com>
2020-07-17 15:32:58 +0530

    Updates

---
+++ precache-manifest.abd892629fd485553ee6b72a6802ad12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655f47f84a6f8075b9020c3aaa2cfaa8",
+    "url": "/projects/index.html"
+  },
+  {
+    "revision": "79581638eeccdac88b8a",
+    "url": "/projects/static/css/main.9d837730.chunk.css"
+  },
+  {
+    "revision": "228ecddcb268b71330e4",
+    "url": "/projects/static/js/2.d9ae5aa2.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/projects/static/js/2.d9ae5aa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79581638eeccdac88b8a",
+    "url": "/projects/static/js/main.7415edbc.chunk.js"
+  },
+  {
+    "revision": "3eb8a72bd82bc117d3bf",
+    "url": "/projects/static/js/runtime-main.b76b2c23.js"
+  }
+]);
